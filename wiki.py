f524d36d23172548fe3336949ea0357aa2b4e9e1
lokal-profil <lokal.profil@gmail.com>
2020-12-09 13:34:56 +0100

    Drop hack needed to support projects outside of programs
    Such projects are no longer supported instead a sanity check is
    added which raises a warning should a program-less project be
    encountered.
    
    Bug: [T242108](https://phabricator.wikimedia.org/T242108)

--- wiki.py
+++ wiki.py
@@ -36,7 +36,7 @@ class Wiki:
         self._year = year
         self._projects = {}
         self._programs = []
-        self._external_projects = []
+        self._all_projects = []
 
     def add_project_page(
             self,
@@ -306,12 +306,6 @@ class Wiki:
 
         config = self._config["year_pages"]["projects"]
         content = ""
-        content += "== {} {} ==\n".format(
-            config["external"]["number"],
-            config["external"]["name"]
-        )
-        for project in self._external_projects:
-            content += self._make_project_data_string(project)
         for program in self._programs:
             content += "== {} {} ==\n".format(
                 program["number"],
@@ -428,7 +422,7 @@ class Wiki:
             table_string,
             flags=re.S
         )
-        self._external_projects = list(self._projects.keys())
+        self._all_projects = list(self._projects.keys())
         # Split table on rows.
         rows = table_string.split("|-")
         for row in rows[1:]:
@@ -472,10 +466,17 @@ class Wiki:
                     self._programs[-1]["strategies"][-1]["projects"].append(
                         project
                     )
-                    self._external_projects.remove(project)
+                    self._all_projects.remove(project)
             # The rightmost cell always contains a goal.
             goal = cells[-1]
             self._programs[-1]["strategies"][-1]["goals"].append(goal)
+        if self._all_projects:
+            logging.warning(
+                "There were projects which could not be matched to programs, "
+                "these will be skipped: '{}'".format(
+                    ', '.join(self._all_projects)
+                )
+            )
 
     def _add_program_overview_year_page(self):
         """Add a page with program overview.
@@ -489,13 +490,6 @@ class Wiki:
         config = self._config["year_pages"]["program_overview"]
         templates = config["templates"]
         content_parameter = ""
-        for project in self._external_projects:
-            content_parameter += Template(
-                templates["project"],
-                True,
-                [project]
-            ).multiline_string()
-            content_parameter += "\n"
         for p, program in enumerate(self._programs):
             content_parameter += Template(
                 templates["program"],
